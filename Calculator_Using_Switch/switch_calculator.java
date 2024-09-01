aa8b26750cc53ca9dfbfb8c07712506cc56524bf
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-09-01 17:31:05 +0800

    added addition

--- Calculator_Using_Switch/switch_calculator.java
+++ Calculator_Using_Switch/switch_calculator.java
@@ -1,4 +1,3 @@
-package Calculator_Using_Switch;
 import java.util.Scanner;
 
 public class switch_calculator{
@@ -7,19 +6,44 @@ public class switch_calculator{
 
         Scanner scanner = new Scanner(System.in);
 
-        String separator = "---------------------------------";
+        String separator = "-------------------------------";
 
         System.out.println(separator);
         System.out.println("Math Modes");
         System.out.println(separator);
-        System.out.println("1. Addition\t\t(+)");
-        System.out.println("2. Subtraction\t\t(-)");
-        System.out.println("3. Multiplication\t(*)");
-        System.out.println("4. Division\t\t(/)");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
         System.out.println(separator);
         
         System.out.print("Enter a mode of calculation: ");
         String math_mode = scanner.nextLine();
         
+        switch (math_mode) {
+            case "1":
+
+                System.out.println("Math Mode Chosen: Addition");
+                System.out.println(separator);
+
+                System.out.print("Enter First Number: ");
+                double first_number = scanner.nextDouble();
+
+                System.out.print("Enter Second Number: ");
+                double second_number = scanner.nextDouble();
+
+                double result = first_number + second_number;
+
+                System.out.println(separator);
+
+                System.out.println(first_number + " + " + second_number + " = " + result);
+
+                System.out.println(separator);
+                
+                break;
+
+            default:
+                System.out.println("INCORRECT MATH MODE!!!");
+        }
     }
 }
