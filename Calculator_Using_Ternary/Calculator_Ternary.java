17a0501620d6fd5b57e7824e3aedf2e8b4850086
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-09-03 21:16:40 +0800

    added some spacing

--- Calculator_Using_Ternary/Calculator_Ternary.java
+++ Calculator_Using_Ternary/Calculator_Ternary.java
@@ -5,10 +5,13 @@ public class Calculator_Ternary {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter first number: ");
             double a = sc.nextDouble();
+
             System.out.print("Enter an operator (+, -, *, /): ");
             char op = sc.next().charAt(0);
+
             System.out.print("Enter second number: ");
             double b = sc.nextDouble();
+            
             double result = (op == '+') ? (a + b) : (op == '-') ? (a - b) : (op == '*') ? (a * b) : (op == '/') ? (a / b) : 0;
             System.out.println("Result: " + result);
         }
