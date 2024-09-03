3b4857f7b9b0e5a934e4f1f82f1920ea2c4293a4
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-09-03 19:20:23 +0800

    Java Calculator that uses less lines as possible

---
+++ Calculator_Mini/Calculator_Mini.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Calculator_Mini {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter first number: ");
+            double a = sc.nextDouble();
+            System.out.print("Enter an operator (+, -, *, /): ");
+            char op = sc.next().charAt(0);
+            System.out.print("Enter second number: ");
+            double b = sc.nextDouble();
+            double result = (op == '+') ? (a + b) : (op == '-') ? (a - b) : (op == '*') ? (a * b) : (op == '/') ? (a / b) : 0;
+            System.out.println("Result: " + result);
+        }
+    }
+}
