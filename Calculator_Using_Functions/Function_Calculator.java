f6deadad135d0603b141ff3bef718c6d925c8a98
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-09-05 20:43:34 +0800

    CalculatorTemplate

---
+++ Calculator_Using_Functions/Function_Calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Function_Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = 0;
+
+        System.out.println("The result is: " + result);
+    }
+}
